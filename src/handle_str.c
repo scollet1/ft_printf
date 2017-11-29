f6f0eaa2677581b414802e234a3559d658ce5c9f
scollet <origamillamaa@gmail.com>
2017-11-28 18:41:38 -0800

    update

--- src/handle_str.c
+++ src/handle_str.c
@@ -12,6 +12,35 @@
 
 #include "ft_printf.h"
 
+void	ft_putwchar_fd(wchar_t chr, int fd)
+{
+	if (chr <= 0x7F)
+		ft_putchar_fd(chr, fd);
+	else if (chr <= 0x7FF)
+	{
+		ft_putchar_fd((chr >> 6) + 0xC0, fd);
+		ft_putchar_fd((chr & 0x3F) + 0x80, fd);
+	}
+	else if (chr <= 0xFFFF)
+	{
+		ft_putchar_fd((chr >> 12) + 0xE0, fd);
+		ft_putchar_fd(((chr >> 6) & 0x3F) + 0x80, fd);
+		ft_putchar_fd((chr & 0x3F) + 0x80, fd);
+	}
+	else if (chr <= 0x10FFFF)
+	{
+		ft_putchar_fd((chr >> 18) + 0xF0, fd);
+		ft_putchar_fd(((chr >> 12) & 0x3F) + 0x80, fd);
+		ft_putchar_fd(((chr >> 6) & 0x3F) + 0x80, fd);
+		ft_putchar_fd((chr & 0x3F) + 0x80, fd);
+	}
+}
+
+void	ft_putwchar(wchar_t chr)
+{
+	ft_putwchar_fd(chr, STDOUT_FILENO);
+}
+
 // void ft_putwchar(wchar_t chr)
 // {
 // 	if (chr <= 0x7F)
