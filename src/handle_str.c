e5012f549413a09cacb7fd75cc8dd13e2d2e6411
scollet <origamillamaa@gmail.com>
2017-11-28 18:39:59 -0800

    update

--- src/handle_str.c
+++ src/handle_str.c
@@ -12,29 +12,29 @@
 
 #include "ft_printf.h"
 
-void ft_putwchar(wchar_t chr)
-{
-	if (chr <= 0x7F)
-		ft_putchar(chr);
-	else if (chr <= 0x7FF)
-	{
-		ft_putchar((chr >> 6) + 0xC0);
-		ft_putchar((chr & 0x3F) + 0x80);
-	}
-	else if (chr <= 0xFFFF)
-	{
-		ft_putchar((chr >> 12) + 0xE0);
-		ft_putchar(((chr >> 6) & 0x3F) + 0x80);
-		ft_putchar((chr & 0x3F) + 0x80);
-	}
-	else if (chr <= 0x10FFFF)
-	{
-		ft_putchar((chr >> 18) + 0xF0);
-		ft_putchar(((chr >> 12) & 0x3F) + 0x80);
-		ft_putchar(((chr >> 6) & 0x3F) + 0x80);
-		ft_putchar((chr & 0x3F) + 0x80);
-	}
-}
+// void ft_putwchar(wchar_t chr)
+// {
+// 	if (chr <= 0x7F)
+// 		ft_putchar(chr);
+// 	else if (chr <= 0x7FF)
+// 	{
+// 		ft_putchar((chr >> 6) + 0xC0);
+// 		ft_putchar((chr & 0x3F) + 0x80);
+// 	}
+// 	else if (chr <= 0xFFFF)
+// 	{
+// 		ft_putchar((chr >> 12) + 0xE0);
+// 		ft_putchar(((chr >> 6) & 0x3F) + 0x80);
+// 		ft_putchar((chr & 0x3F) + 0x80);
+// 	}
+// 	else if (chr <= 0x10FFFF)
+// 	{
+// 		ft_putchar((chr >> 18) + 0xF0);
+// 		ft_putchar(((chr >> 12) & 0x3F) + 0x80);
+// 		ft_putchar(((chr >> 6) & 0x3F) + 0x80);
+// 		ft_putchar((chr & 0x3F) + 0x80);
+// 	}
+// }
 
 ssize_t	ft_printf_handle_char(char **format, va_list *args, t_data *data)
 {
