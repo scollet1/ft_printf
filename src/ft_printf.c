f685d3147f506f9239c70e59c399bbc4a5be35bc
scollet <origamillamaa@gmail.com>
2017-11-30 14:20:11 -0800

    g

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -19,16 +19,16 @@ int             handle_args(char **format, va_list *args, t_data *data)
   t_handler       f;
 
   if (!(*(++*format)))
-          return (0);
+    return (0);
   if (!(ft_printf_parse_flags(format, data)) ||
       !(ft_printf_parse_width(format, args, data)) ||
       !(ft_printf_parse_accuracy(format, args, data)) ||
       !(ft_printf_parse_length(format, data)))
     return (-1);
   if (!**format)
-          return (0);
+    return (0);
   if (!(f = ft_printf_get_handler(**format)))
-          f = &ft_printf_handle_null;
+    f = &ft_printf_handle_null;
   ret = f(format, args, data);
   (*format)++;
   return (ret);
@@ -43,18 +43,18 @@ int             go_printf(const char *format, va_list *args, int length)
   len = 0;
   next = ft_strchr(format, '%');
   if (!*format)
-          return (length);
+    return (length);
   if (!next)
-          return (length + write(1, format, ft_strlen(format)));
+    return (length + write(1, format, ft_strlen(format)));
   else if (next > format)
-          return (go_printf(next, args, length + write(1, format,
+    return (go_printf(next, args, length + write(1, format,
                                           next - format)));
   else
   {
-          ft_bzero(&this_data, sizeof(this_data));
-          if ((len = handle_args((char**)&format, args, &this_data)) == -1)
-                  return (-1);
-          return (go_printf(format, args, length + len));
+    ft_bzero(&this_data, sizeof(this_data));
+    if ((len = handle_args((char**)&format, args, &this_data)) == -1)
+      return (-1);
+    return (go_printf(format, args, length + len));
 	}
 	return (-1);
 }
